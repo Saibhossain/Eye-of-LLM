b4394f6f1a171b889a1afa952b905224a0bf6b81
mdsaibhossain <saibhossain5@gmail.com>
2025-12-18 18:30:57 +0600

    camera

---
+++ app/camera.py
@@ -0,0 +1,17 @@
+import cv2
+
+def capture_image():
+    cap = cv2.VideoCapture(0)
+
+    if not cap.isOpened():
+        raise RuntimeError("Cannot access camera")
+
+    ret, frame = cap.read()
+    cap.release()
+
+    if not ret:
+        raise RuntimeError("Failed to capture image")
+
+    return frame
+
+capture_image()
